ca8c385631f552753cb1a3157b43d4cb0ea72989
nelsonalejadrosaz <nelsonalejandrosaz@gmail.com>
2021-12-13 17:51:21 -0600

    Agregar archivos de proyecto.

---
+++ SIGAUREF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIGAUREF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
